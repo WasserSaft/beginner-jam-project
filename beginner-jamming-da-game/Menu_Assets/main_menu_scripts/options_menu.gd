cdd75c9ef86da332397f9d16e84b7ceef7719a7e
7critters <savannahthompson2005@gmail.com>
2025-03-28 00:23:31 -0700

    very broken but i am sleepy

---
+++ beginner-jamming-da-game/Menu_Assets/main_menu_scripts/options_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+class_name OptionsMenu
+
+@onready var exit_button: Button = $MarginContainer/VBoxContainer/exit_button
+
+
+signal exit_options_menu
+
+func _ready():
+	exit_button.button_down.connect(on_exit_pressed)
+	set_process(false)
+	visible = false
+
+func on_exit_pressed() -> void:
+	exit_options_menu.emit()
+	set_process(false)
