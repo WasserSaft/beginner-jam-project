c750b0a90d57f37e081ef6e74c7a0e8dee12f151
7critters <savannahthompson2005@gmail.com>
2025-04-01 22:32:54 -0700

    tweaks

--- beginner-jamming-da-game/Scenes/interactable objects/mirror.gd
+++ beginner-jamming-da-game/Scenes/interactable objects/mirror.gd
@@ -8,7 +8,7 @@ var player_ref: Node
 @onready var dialog_lines := [
 	["Uncle", "No cracks in this mirror. But that’s it, that’s the gist of it. It’s how they get in…"],
 	["Nephew", "Who? Who gets in? What are you talking about?"],
-	["Uncle", "The mirror world, kid. You stare into a mirror too long, they start showing up. It’s why mirrors are bad luck."],
+	["Uncle", "The mirror world, kid. You stare into a mirror too long, an those blurry figures start messing your face up... It’s why mirrors are bad luck."],
 	["Uncle", "That’s why this circus is traveling around… they don’t want the government to see what they’re building."],
 	["Nephew", "You need a mental evaluation! Who is “they” in this context!?"],
 	["Uncle", "Oh yeah, put this in your pocket for me will you?"],
